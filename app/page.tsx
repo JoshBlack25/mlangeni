1c5aca4cbd773677d12aeedca2bc79642010572f
mogamatyaseenkannemeyer <yaseenkannemeyer@gmail.com>
2026-04-16 10:12:40 +0200

    chore: clean up empty code change sections in the changes log

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import Gallery from "./sections/Gallery";
 import Ratings from "./sections/Ratings";
 import About from "./sections/About";
 import Contact from "./sections/Contact";
+import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
     <>
+      <NavBar />
       <Hero />
       <Calendar />
       <Gallery />
