3f47748d19e9fa12fe65c5df3eab08a76d691399
Ryle <rylemay198@gmail.com>
2026-05-15 14:59:53 +0200

    removed unnecessary comments in gallery hero

--- components/gallery/Hero.jsx
+++ components/gallery/Hero.jsx
@@ -1,7 +1,6 @@
 import IMAGES from "../constants/image";
 
 function Hero() {
-    // const image = "../images/Gallery-Image1.png";
     return (
         <section className="relative h-screen">
             <img
@@ -9,7 +8,6 @@ function Hero() {
                 alt="A beautifully set dining table"
                 className="w-full h-full object-cover"
             />
-            {/* Dark scrim so the heading is always legible */}
             <div className="absolute inset-0 bg-black/40" />
             <h1
                 className="absolute inset-0 flex items-center justify-center
