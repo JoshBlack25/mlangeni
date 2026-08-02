9aaa1b58d94a50c013f58d33f71182d10d20bfd7
Ryle <rylemay198@gmail.com>
2026-08-02 18:08:30 +0200

    fixing error

--- app/menu/page.jsx
+++ app/menu/page.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { supabase } from "@/services/supabaseClient.ts";
+import { supabase } from "@/services/supabaseClient";
 import { PackageCalendar } from "@/app/components/PackageCalendar";
 import {
   Carousel,
